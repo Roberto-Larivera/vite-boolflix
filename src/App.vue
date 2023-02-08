6f3c857973a28d3550cb39248cc11be67d34bb6e
Roberto Larivera <robylariv@gmail.com>
2023-02-08 12:11:29 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+import { store } from './store.js';
+import AppHeader from './components/Header/AppHeader.vue';
+import AppMain from './components/Main/AppMain.vue';
+import AppFooter from './components/Footer/AppFooter.vue';
+export default{
+  name: 'App',
+  components:{
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+  data(){
+    return{
+      store,
+    }
+  },
+  
+}
+</script>
+
+<template>
+  <div>
+    <AppHeader/>
+    <AppMain/>
+    <AppFooter/>
+  </div>
+</template>
+
+<style lang="scss">
+@import './style/main.scss';
+</style>
