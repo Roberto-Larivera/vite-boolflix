4b7d3d5cb5510a74a4ac9d06e690578f68d29aa3
Roberto Larivera <robylariv@gmail.com>
2023-02-09 13:33:04 +0100

    m.4 style main - card

--- src/components/Main/AppCard.vue
+++ src/components/Main/AppCard.vue
@@ -6,28 +6,28 @@ export default {
 
     }
   },
-  props : {
-    cardTitle :{
+  props: {
+    cardTitle: {
       type: String,
       default: 'Hei card!'
     },
-    cardOriginalTitle :{
+    cardOriginalTitle: {
       type: String,
       default: 'Hei card original!'
     },
-    cardPosterPath :{
+    cardPosterPath: {
       type: String,
       default: '...'
     },
-    cardOriginalLanguage :{
+    cardOriginalLanguage: {
       type: String,
       default: '...'
     },
-    cardVoteAverage :{
+    cardVoteAverage: {
       type: Number,
       default: 0
     },
-    cardFlag:{
+    cardFlag: {
       type: String,
       default: '...'
     }
@@ -38,39 +38,29 @@ export default {
 </script>
 
 <template>
-    <div class="card">
-      <ul>
-        <li>
-          {{ cardTitle }}
-        </li>
-        <li>
-          {{ cardOriginalTitle }}
-        </li>
-        <li v-if="cardPosterPath != null">
-          <img :src="`http://image.tmdb.org/t/p/w342/${cardPosterPath}`"
-            :alt="cardOriginalTitle">
-        </li>
-        <li v-else>
-          <img src="https://via.placeholder.com/342.png/fff000/fff000"
-            :alt="cardOriginalTitle">
-        </li>
-        <li>
-          {{ cardOriginalLanguage }}
-          <span :class="`fi fi-${cardFlag}`"></span>
-        </li>
-        <li>
-          {{ cardVoteAverage }}
-          <span>
-            <font-awesome-icon :icon="['fas', 'star']" v-for="n in cardVoteAverage" />
-            <font-awesome-icon :icon="['far', 'star']" v-for="n in (5 - cardVoteAverage)" />
-            <!-- <font-awesome-icon icon="fa-regular fa-star" /> -->
-          </span>
-
-        </li>
-      </ul>
+  <div class="card w-100">
+    <img class="card-img w-100 h-100 obj-fit" :src="`http://image.tmdb.org/t/p/w342/${cardPosterPath}`" :alt="cardOriginalTitle" v-if="cardPosterPath != null">
+    <img class="card-img w-100 h-100 obj-fit" src="https://via.placeholder.com/342X500.png/fff000/fff000" :alt="cardOriginalTitle" v-else>
+    <div class="card-img-overlay">
+      <h5 class="card-title">{{ cardTitle }}</h5>
+      <h5 class="card-title">{{ cardOriginalTitle }}</h5>
+      <p class="card-text">{{ cardOriginalLanguage }}</p>
+      <span :class="`fi fi-${cardFlag}`"></span>
+      <p class="card-text"><small>{{ cardVoteAverage }}</small></p>
+      <span>
+          <font-awesome-icon :icon="['fas', 'star']" v-for="n in cardVoteAverage" />
+          <font-awesome-icon :icon="['far', 'star']" v-for="n in (5 - cardVoteAverage)" />
+          <!-- <font-awesome-icon icon="fa-regular fa-star" /> -->
+        </span>
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
-
+.card{
+  &:hover.card-img-overlay
+  .obj-fit{
+    object-fit: cover;
+  }
+}
 </style>
--- src/components/Main/AppMain.vue
+++ src/components/Main/AppMain.vue
@@ -5,20 +5,20 @@ import AppCard from './AppCard.vue';
 
 export default {
   name: 'AppMain',
-  components:{
+  components: {
     AppCard,
   },
-  data(){
-    return{
+  data() {
+    return {
       store,
     }
   },
   methods: {
     trasformNumber(number) {
-      return Math.round( Math.ceil(number) / 2)
+      return Math.round(Math.ceil(number) / 2)
     },
-    checkFlag(lang){
-      switch (lang){
+    checkFlag(lang) {
+      switch (lang) {
 
         case 'en':
           lang = 'gb'
@@ -32,47 +32,40 @@ export default {
           break;
 
       }
-      console.log(lang,'lang')
+      console.log(lang, 'lang')
       const flag = lang
-      console.log(flag,'flag')
+      console.log(flag, 'flag')
       return flag
     }
   },
-  
+
 }
 </script>
 
 <template>
-  <div>
-    <h2>
-      film
-    </h2>
-    <div class="card" v-for="(element, index) in this.store.searchListMovie">
-      <AppCard
-        :cardTitle="element.title "
-        :cardOriginalTitle=" element.original_title"
-        :cardPosterPath=" element.poster_path"
-        :cardOriginalLanguage=" element.original_language "
-        :cardFlag=" checkFlag(element.original_language) "
-        :cardVoteAverage="trasformNumber(element.vote_average)"
-      />
-    </div>
+  <main>
+    <div class="container">
+      <div class="row row-cols-5">
+        <div class="col d-flex align-items-stretch" v-for="(element, index) in this.store.searchListMovie">
+          <AppCard :cardTitle="element.title" :cardOriginalTitle="element.original_title"
+            :cardPosterPath="element.poster_path" :cardOriginalLanguage="element.original_language"
+            :cardFlag="checkFlag(element.original_language)"
+            :cardVoteAverage="trasformNumber(element.vote_average)" />
+        </div>
+      </div>
+      <div class="row row-cols-5">
+        <div class="col d-flex align-items-stretch" v-for="(element, index) in this.store.searchListTv">
+          <AppCard :cardTitle="element.name" :cardOriginalTitle="element.original_name"
+            :cardPosterPath="element.poster_path" :cardOriginalLanguage="element.original_language"
+            :cardFlag="checkFlag(element.original_language)"
+            :cardVoteAverage="trasformNumber(element.vote_average)" />
+        </div>
+      </div>
 
-    <h2>
-      tv
-    </h2>
-    <div class="card" v-for="(element, index) in this.store.searchListTv">
-      <AppCard
-        :cardTitle="element.name "
-        :cardOriginalTitle=" element.original_name"
-        :cardPosterPath=" element.poster_path"
-        :cardOriginalLanguage=" element.original_language "
-        :cardFlag=" checkFlag(element.original_language) "
-        :cardVoteAverage="trasformNumber(element.vote_average)"
-      />
     </div>
-  </div>
+  </main>
 </template>
 
 <style lang="scss" scoped>
+
 </style>
